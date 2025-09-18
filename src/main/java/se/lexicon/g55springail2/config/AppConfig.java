0b8614946549f8ea393544d1c0f3e625b1c1fb7c
Mehrdad Javan <mehrdad.javan@lexicon.se>
2025-09-18 11:41:11 +0200

    store chat info to mysql database

--- src/main/java/se/lexicon/g55springail2/config/AppConfig.java
+++ src/main/java/se/lexicon/g55springail2/config/AppConfig.java
@@ -1,19 +1,41 @@
 package se.lexicon.g55springail2.config;
 
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.chat.memory.repository.jdbc.MysqlChatMemoryRepositoryDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @ComponentScan("se.lexicon.g55springail2.*")
 public class AppConfig {
 
-    @Bean
+    /*@Bean
     public ChatMemory chatMemory(){
         return MessageWindowChatMemory.builder()
                 .maxMessages(10)
                 .build();
+    }*/
+
+    @Bean
+    public ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository){
+        return MessageWindowChatMemory.builder()
+                .maxMessages(10)
+                .chatMemoryRepository(chatMemoryRepository)
+                .build();
+    }
+
+    @Bean
+    public ChatMemoryRepository chatMemoryRepository(JdbcTemplate jdbcTemplate){
+        return JdbcChatMemoryRepository.builder()
+                .jdbcTemplate(jdbcTemplate)
+                .dialect(new MysqlChatMemoryRepositoryDialect())
+                .build();
+
+
     }
 }
